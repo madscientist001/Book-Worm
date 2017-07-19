c2fdca64a4504336abe0a72124618b80521efef1
Sourabh Sethi <sourabhsethi1000@gmail.com>
2017-07-19 18:56:13 +0530

    added Basic SignUp layout

--- app/src/main/java/grimreaper/bookworm/MainActivity.java
+++ app/src/main/java/grimreaper/bookworm/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnFr
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         FragmentManager manager = (FragmentManager)getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
--- app/src/main/java/grimreaper/bookworm/SignUpFragment.java
+++ app/src/main/java/grimreaper/bookworm/SignUpFragment.java
@@ -1,14 +1,24 @@
 package grimreaper.bookworm;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SignUpFragment extends Fragment {
+public class SignUpFragment extends Fragment implements View.OnClickListener{
 
+    private String signUpName;
+    private String signUpEmail;
+    private String signUpPassword;
+    private EditText emailEdit , passwordEdit;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -19,7 +29,71 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        Button signUpButton = (Button) rootView.findViewById(R.id.signUpUsingEmail);
+        signUpButton.setOnClickListener(this);
+        emailEdit = (EditText)rootView.findViewById(R.id.editEmail);
+        passwordEdit = (EditText)rootView.findViewById(R.id.editPassword);
+        return rootView;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.signUpUsingEmail:
+                signUpEmail = emailEdit.getText().toString();
+                signUpPassword = passwordEdit.getText().toString();
+                int isValid = checkLoginCredentials(signUpEmail , signUpPassword);
+                if(isValid == 1)
+                    showDialog("Invalid Email Address");
+                else if(isValid == 2)
+                    showDialog("Password must be 4-20 characters");
+                else
+                    Log.d("goku" , "Signed Up"); //createUserAccout(signUpEmail , signUpPassword);
+                break;
+        }
+    }
+
+    void showDialog(String errorMessage){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("BookWorm")
+                .setMessage(errorMessage)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //do nothing
+                    }
+                })
+                .show();
+    }
+
+    int checkLoginCredentials(String email , String password){
+        boolean validEmail = validateEmail(email);
+        boolean validPassword = validatePassword(password);
+        if(!validEmail){
+            Log.d("goku", "Invalid Email");
+            return 1;
+        }else if(!validPassword){
+            Log.d("goku", "Invalid Password");
+            return 2;
+        }else{
+            Log.d("goku","Signed Up");
+            return 3;
+        }
+    }
+
+    boolean validateEmail(String email){
+        boolean isValid = false , flag = false;
+        for(int i = 0 ; i < email.length() ; i++){
+            if(email.charAt(i) == '@')
+                flag = true;
+            if(flag && email.charAt(i) == '.')
+                isValid = true;
+        }
+        return isValid;
+    }
+
+    boolean validatePassword(String password){
+        return (password.length() >= 4 && password.length() <= 20);
+    }
 }
